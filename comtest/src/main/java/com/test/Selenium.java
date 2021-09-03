66b844c1b50bfc41dfe29edc27afd27dfec0aba0
lakshmi4025 <88842239+lakshmi4025@users.noreply.github.com>
2021-09-03 04:01:29 -0700

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Selenium {
 
     public static void main(String[] args) {
-      [...]
+     
 public Map initCrawler(Things that I give as input) throws InterruptedException, ApiException, IOException {
     System.setProperty("webdriver.chrome.driver",  BackEndProperties.getProperty("chrome_driver_path") );
     final ChromeOptions chromeOptions = new ChromeOptions();
@@ -14,7 +14,7 @@ public Map initCrawler(Things that I give as input) throws InterruptedException,
     chromeOptions.addArguments("--disable-extensions");
     chromeOptions.addArguments("--disable-gpu");
     chromeOptions.addArguments("--no-sandbox");
-    [...]
+   
 
         driver.get(baseUrl);
         actualTitle = driver.getTitle();
